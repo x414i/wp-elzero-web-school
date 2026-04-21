22beec7cab31212dee8a0275517dfc1d54e36fa6
x414i <m.belaid@lamah.com>
2026-04-21 12:56:54 +0200

    Add enqueue for Font Awesome stylesheet in custom theme

--- wp-content/themes/custom-theme/functions.php
+++ wp-content/themes/custom-theme/functions.php
@@ -5,7 +5,10 @@ function custom_theme_enqueue_styles() {
     wp_enqueue_style('custom-theme-style', get_stylesheet_uri());
     // Enqueue the custom stylesheet
     wp_enqueue_style('custom-theme-custom-style', get_template_directory_uri() . '/css/main.css', array(),'1.0','all');
-}
+// enqueue fontawesome
+
+wp_enqueue_style('font-awesome', get_template_directory_uri() . '/css/all.min.css');
+    }
 // Hook the function to the 'wp_enqueue_scripts' action
 add_action('wp_enqueue_scripts', 'custom_theme_enqueue_styles');
 

8a919bb2c75fb64cbd438d32d87789b670dee72f
x414i <m.belaid@lamah.com>
2026-04-21 12:56:59 +0200

    Add Google Fonts preconnect and stylesheet links for improved typography

--- wp-content/themes/custom-theme/header.php
+++ wp-content/themes/custom-theme/header.php
@@ -9,6 +9,13 @@
     ?></title>
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 <?php wp_head(); // Hook for plugins and additional head elements ?>
+
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&family=Tajawal:wght@200;300;400;500;700;800;900&display=swap" rel="stylesheet">
+
 </head>
 <body>
-    
+    <i class="fa-brands fa-affiliatetheme"></i>
